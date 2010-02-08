766190a066ff439af3a2901a8c2c57e0f70b6d14
djculex <djculex@c17157d5-08f2-41e7-a131-df055f6a0da3>
2010-02-08 18:17:41 +0000

    Commented the files adding indexfiles

--- blocks/b_instantmessenger.php
+++ blocks/b_instantmessenger.php
@@ -1,4 +1,36 @@
 <?php
+// $Id: b_instantmessenger.php,v 1.oo (alpha 1) 08/02/2010 20:00:00 culex $
+// Inspired and referenced by Anant Garg (anantgar.com (c) 2009
+//  ------------------------------------------------------------------------ //
+//                XOOPS - PHP Content Management System                      //
+//                  Copyright (c) 2000-2010 XOOPS.org                        //
+//                       <http://www.xoops.org/>                             //
+//  ------------------------------------------------------------------------ //
+//  This program is free software; you can redistribute it and/or modify     //
+//  it under the terms of the GNU General Public License as published by     //
+//  the Free Software Foundation; either version 2 of the License, or        //
+//  (at your option) any later version.                                      //
+//                                                                           //
+//  You may not change or alter any portion of this comment or credits       //
+//  of supporting developers from this source code or any supporting         //
+//  source code which is considered copyrighted (c) material of the          //
+//  original comment or credit authors.                                      //
+//                                                                           //
+//  This program is distributed in the hope that it will be useful,          //
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of           //
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            //
+//  GNU General Public License for more details.                             //
+//                                                                           //
+//  You should have received a copy of the GNU General Public License        //
+//  along with this program; if not, write to the Free Software              //
+//  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA //
+//  ------------------------------------------------------------------------ //
+
+
+/*
+* Main block function. Created from the online system block
+* Adds an "who is online" block with a link to chat with registered user.
+*/
 function b_instantmessenger() {
     global $xoopsUser, $xoopsModule;
 	$online_handler =& xoops_gethandler('online');
@@ -29,13 +61,8 @@ function b_instantmessenger() {
 		$Online_img = "<img src='".XOOPS_URL."/modules/instantmessenger/images/online.png' height='16px' width='16px'></img>";
         for ($i = 0; $i < $total; $i++) {
             if ($onlines[$i]['online_uid'] > 0) {
-				$membername[$i] = $onlines[$i]['online_uname'];
-				//$members .= "<a href='javascript:void(0)' onClick='javascript:chatWith($membername)'>" . htmlspecialchars($onlines[$i]['online_uname']) . "</a><br>";
-				//$members .= '<a href="javascript:void(0)" onclick="javascript:chatWith(\''.$membername.'\')">Chat With '.$uname.'</a>';
-				
-			$members .= '<span style="position:relative;left:15px;">'.$Online_img.'<a href="javascript:void(0)" onclick="javascript:chatWith(\''. htmlentities($membername[$i]) .'\')">'.htmlentities($membername[$i]).'</a></span><br />';
-				
-				
+			$membername[$i] = $onlines[$i]['online_uname'];
+				$members .= '<span style="position:relative;left:15px;">'.$Online_img.'<a href="javascript:void(0)" onclick="javascript:chatWith(\''. htmlentities($membername[$i]) .'\')">'.htmlentities($membername[$i]).'</a></span><br />';
             } else {
                 $guests++;
             }
--- xoops_version.php
+++ xoops_version.php
@@ -1,13 +1,40 @@
 <?php
+// $Id: b_instantmessenger.php,v 1.oo (alpha 1) 08/02/2010 20:00:00 culex $
+// Inspired and referenced by Anant Garg (anantgar.com (c) 2009
+//  ------------------------------------------------------------------------ //
+//                XOOPS - PHP Content Management System                      //
+//                  Copyright (c) 2000-2010 XOOPS.org                        //
+//                       <http://www.xoops.org/>                             //
+//  ------------------------------------------------------------------------ //
+//  This program is free software; you can redistribute it and/or modify     //
+//  it under the terms of the GNU General Public License as published by     //
+//  the Free Software Foundation; either version 2 of the License, or        //
+//  (at your option) any later version.                                      //
+//                                                                           //
+//  You may not change or alter any portion of this comment or credits       //
+//  of supporting developers from this source code or any supporting         //
+//  source code which is considered copyrighted (c) material of the          //
+//  original comment or credit authors.                                      //
+//                                                                           //
+//  This program is distributed in the hope that it will be useful,          //
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of           //
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            //
+//  GNU General Public License for more details.                             //
+//                                                                           //
+//  You should have received a copy of the GNU General Public License        //
+//  along with this program; if not, write to the Free Software              //
+//  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA //
+//  ------------------------------------------------------------------------ //
+
 $modversion['name'] = "IM";
-$modversion['version'] = 1.00;
+$modversion['version'] = "1.00 (alpha 1)";
 $modversion['description'] = "Instant Messenger";
 $modversion['author'] = "Culex";
-$modversion['credits'] = "Culex";
+$modversion['credits'] = "Original script by Anant Garg";
 $modversion['help'] = "";
 $modversion['license'] = "GPL see LICENSE";
 $modversion['official'] = 0;
-$modversion['image'] = "images/tutorial.png";
+$modversion['image'] = "images/instantmessenger.png";
 $modversion['dirname'] = "instantmessenger";
 
 // Admin
